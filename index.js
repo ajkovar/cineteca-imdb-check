5845eb6551a0d422c0d6f2f109083af1c3c27067
Alex Kovar <alexkovar@MacBook-Air-de-Alex.local>
2024-01-15 13:48:38 -0600

    initial working version

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
 import fetch from 'node-fetch';
 import { parse } from 'node-html-parser';
 
-console.log('start')
+console.log('Querying info...')
 
-fetch('https://www.cinetecanacional.net/cartelera.php?dia=2024-01-15#gsc.tab=0')
-  .then((response) => response.text())
-  .then((data) => {
-    const root = parse(data)
-    // console.log(data);
-    const titles = root.querySelectorAll('.font-weight-bold.text-uppercase.text-decoration-none.text-black').map((n) => n.text)
-    console.log(titles);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+function cinetecaElToSearchString(el) {
+  const title = el.querySelector('.font-weight-bold.text-uppercase.text-decoration-none.text-black').text
+  const description = el.querySelector('div.small').text
+  const yearRegex = /\b\d{4}\b/;
+  const yearMatch = description.match(yearRegex);
 
-function toText(el) {
-  const title = Array.from(el.querySelectorAll('.font-weight-bold.text-uppercase.text-decoration-none.text-black'))[0].text
-  const description = Array.from(el.querySelectorAll('.small'))[0].text
-  console.log(title)
-  return title
+  // const directorRegex = /Dir\.: (.+?)(?:,|$)/;
+  // const directorMatch = description.match(directorRegex);
+
+  return title + ' ' + yearMatch[0] // + ' ' + directorMatch[1]
 }
+
+async function searchImdb(searchString) {
+  return getRootElFromUrl('https://www.imdb.com/find/?q=' + encodeURIComponent(searchString))
+}
+
+function getFirstLinkOnPage(root) {
+  const firstLink = root.querySelector('.ipc-metadata-list-summary-item__t')
+  return 'https://www.imdb.com/' + firstLink.getAttribute('href')
+}
+
+async function getRootElFromUrl(url) {
+  return parse(await (await fetch(url)).text())
+}
+
+async function getScoreFromUrl(url) {
+  const root = await getRootElFromUrl(url)
+  const el = root.querySelector(".sc-bde20123-1.cMEQkK")
+  return el ? el.innerHTML : undefined
+}
+
+const root = await getRootElFromUrl('https://www.cinetecanacional.net/cartelera.php?dia=2024-01-15#gsc.tab=0')
+const titles = await Promise.all(root.querySelectorAll('.col-12.col-md-6.col-lg-4.float-left').map(cinetecaElToSearchString).map(async (searchString) => {
+  const score = await getScoreFromUrl(getFirstLinkOnPage(await searchImdb(searchString)))
+  return {
+    score, searchString
+  }
+}))
+
+console.log(titles.sort((m1, m2) => m1.score - m2.score))
+
+
